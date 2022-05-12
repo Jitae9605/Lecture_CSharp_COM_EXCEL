61273f4c6f778742b020e91f08a122daf3439b29
Jitae9605 <dlwlxo3819@naver.com>
2022-05-12 09:48:44 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace COM_EXCEL_Practice
+{
+	class MainApp
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
